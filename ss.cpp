8d7801c4c855239cc2ffba292e648bb74f6e733e
maryam abbasi <169179074+mary6776abbsi@users.noreply.github.com>
2024-06-12 19:45:53 +0330

    ss

---
+++ ss.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numStars;
+    cout << "Enter the number of stars: ";
+    cin >> numStars;
+
+    // چاپ بخش بالایی شکل
+    for (int i = 0; i < numStars; ++i) {
+        for (int j = 0; j < numStars; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // چاپ خط خالی
+    for (int i = 0; i < numStars; ++i) {
+        cout << endl;
+    }
+
+    // چاپ بخش پایینی شکل
+    for (int i = 0; i < numStars; ++i) {
+        for (int j = 0; j < numStars; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
